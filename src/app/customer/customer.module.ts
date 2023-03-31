2038b5c7e8468eea0c2107fc79cc8af6e1c9e27e
huynhgiahuy0409 <76533370+huynhgiahuy0409@users.noreply.github.com>
2023-03-31 12:39:25 +0700

    demo

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -5,11 +5,13 @@ import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { CustomerLayoutModule } from '../shared/layout/customer-layout/customer-layout.module';
 import { AngularMaterialModule } from '../shared/angular-material/angular-material.module';
+import { DemoComponent } from './demo/demo.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    DemoComponent
   ],
   imports: [
     CommonModule,
