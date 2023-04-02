48844a4a35ba96d4c3003b954e58a39e4fd7700c
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2023-04-02 18:51:40 +0700

    update-create-basic-info-of-lease

--- src/app/lease/create/location/location.component.ts
+++ src/app/lease/create/location/location.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-location',
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
-  constructor() { }
+  public countryData = [
+    {country: 'Choose country',},
+    {
+      country: 'Viet Nam', city: [
+        'Ha Noi',
+        'Tp Hcm',
+        'Da Nang'
+      ]
+    },
+    {
+      country: 'Thai land', city: [
+        'Thai1',
+        'thai2',
+        'Da Nang'
+      ]
+    }
+  ]
+  public city: string[] = [];
+
+  changeCountry(event: any) {
+    const country = event.target.value;
+    console.log(country)
+    if (!country) return;
+    this.city = this.countryData.find((data) => data.country === country)?.city || [];
+
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
