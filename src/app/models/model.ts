fe1c5a50321b24515ca14851e5a3da7fe934131a
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2023-04-11 18:19:03 +0700

    update model

--- src/app/models/model.ts
+++ src/app/models/model.ts
@@ -37,3 +37,81 @@ export interface RedirectInfo {
     path: string,
     icon?: string
 }
+
+export interface Basic {
+
+  typeComparisionBusiness: string;
+  numComparisionBusiness: number;
+  numAccommodations: number;
+  numBathroom: number;
+  numBedroom: number;
+  numRoomOne: number;
+  typeRoomOne: string;
+  numCommon: number;
+  typeCommon: string;
+}
+
+export interface Location {
+
+  address: string;
+  house: string;
+  country: string;
+  city: string;
+}
+
+export interface Description {
+  nameHouse: string;
+  descHouse: string;
+  suggest: string;
+  rule: string;
+  move: string;
+  star: number;
+}
+
+export interface Amenities {
+  recomendation: string[];
+}
+
+export interface MySelf {
+  firstName: string;
+  lastName: string;
+  date: string;
+}
+
+export interface Company {
+  nameCompany: string;
+
+
+  addressCompany: string;
+
+
+  codeAreaCompany: string;
+}
+
+export interface Photos {
+  file: string;
+  fileSource: string[];
+}
+
+export interface Pricing {
+  managerChannel: string;
+  payment: string;
+  price: number;
+}
+
+export interface Profile {
+  typeHost: string;
+  company: Company;
+  mySelf: MySelf;
+}
+
+export interface HotelProfile {
+  id:number  ;
+  basic: Basic;
+  location: Location;
+  description: Description;
+  amenities: Amenities;
+  pricing: Pricing;
+  photos: Photos;
+  profile: Profile;
+}
